d0ff93f5cab7e5e93bb1fc10924ec0f7d02ea7df
Thomas Fritchman <tcfritchman@gmail.com>
2019-07-10 20:40:50 -0700

    Clean up

--- emulator.c
+++ emulator.c
@@ -6,9 +6,9 @@
 
 unsigned char TEST_BYTES[] = {0x78, 0xB1, 0xC8, 0x1A, 0x77, 0x13, 0x23, 0x0B, 0x78, 0xB1, 0xC2, 0x03, 0x10, 0xC9};
 
-void update_state(ProcState state) {
-  unsigned short pc = get_pc(state);
-  OpCode op_code = OP_CODES[get_mem_byte(state, pc)];
+void update_state(ProcState *state) {
+  unsigned short pc = state->pc;
+  OpCode op_code = OP_CODES[state->mem[pc]];
 
   switch (op_code.code) {
 
@@ -22,34 +22,23 @@ void update_state(ProcState state) {
 
     case 0x37:
       // STC
-      set_carry(state, 1);
       break;
     case 0x3f:
       // CMC
-      set_carry(state, !get_carry(state));
       break;
 
     // Single register instructions
 
     case 0x04:
       // INR B
-      set_reg_b(state, get_reg_b(state) + 1);
-      set_parity(state, parity(get_reg_b(state)));
-      set_zero(state, zero(get_reg_b(state)));
-      set_sign(state, sign(get_reg_b(state)));
       break;
 
     case 0x05:
       // DCR B
-      set_reg_b(state, get_reg_b(state) - 1);
-      set_parity(state, parity(get_reg_b(state)));
-      set_zero(state, zero(get_reg_b(state)));
-      set_sign(state, sign(get_reg_b(state)));
       break;
 
     case 0x2f:
       // CMA
-      set_reg_a(state, complement(get_reg_a(state)));
       break;
 
     case 0x27:
@@ -65,85 +54,71 @@ void update_state(ProcState state) {
       break;
     case 0x41:
       // MOV B,C
-      set_reg_b(state, get_reg_c(state));
       break;
     case 0x42:
       // MOV B,D
-      set_reg_b(state, get_reg_d(state));
       break;
     case 0x43:
       // MOV B,E
-      set_reg_b(state, get_reg_e(state));
       break;
     case 0x44:
       // MOV B,H
-      set_reg_b(state, get_reg_h(state));
       break;
     //....
 
     case 0x02:
       // STAX B
-      set_mem_byte(state, get_reg_pair_b_c(state), get_reg_a(state));
       break;
 
     case 0x0a:
       // LDAX B
-      set_reg_a(state, get_mem_byte(state, get_reg_pair_b_c(state)));
       break;
 
     // Register or Memory to Accumulator instructions
     case 0x80:
       // ADD B
-      add(state.reg_b, &state);
       break;
 
       //...
 
     case 0x88:
       // ADC B
-      adc(state.reg_b, &state);
       break;
 
       //...
 
     case 0x90:
       // SUB B
-      sub(state.reg_b, &state);
       break;
 
       //...
 
     case 0x98:
       // SBB B
-      sbb(state.reg_b, &state);
       break;
 
       //...
 
     case 0xa0:
       // ANA B
-      ana(state.reg_b, &state);
       break;
 
       //...
     
     case 0xa8:
       // XRA B
-      xra(state.reg_b, &state);
       break;
 
       //...
 
     case 0xb0:
       // ORA B
-      ora(state.reg_b, &state);
       break;
 
       //...
 
     case 0xb8:
       // CMP B
-      cmp(state.reg_b, &state);
       break;
 
     case 0x76:
@@ -151,7 +126,7 @@ void update_state(ProcState state) {
       return;
   }
 
-  set_pc(state, pc + op_code.size);
+  state->pc += op_code.size;
 }
 
 int main(int argc, char const *argv[]) {
